7164b105bef7acdca2f143fec6c3dbd919cdb196
lucasgian <gianextensao@gmail.com>
2018-03-25 15:53:32 -0400

    feat (View/Page) add home.php (#14)

---
+++ src/View/Pages/home.php
@@ -0,0 +1,163 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         0.10.0
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+
+$cakeDescription = 'CakePHP: the rapid development PHP framework';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?php //$this->Html->charset() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= $cakeDescription ?>
+    </title>
+
+    <?php //$this->Html->meta('icon') ?>
+    <?php //$this->Html->css('home.css') ?>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:500i|Roboto:300,400,700|Roboto+Mono" rel="stylesheet">
+</head>
+<body class="home">
+
+<header class="row">
+</header>
+
+<div class="row">
+    <div class="columns large-12">
+        <div class="ctp-warning alert text-center">
+            <p>Please be aware that this page will not be shown if you turn off debug mode unless you replace src/Template/Pages/home.ctp with your own version.</p>
+        </div>
+        <div id="url-rewriting-warning" class="alert url-rewriting">
+            <ul>
+                <li class="bullet problem">
+                    URL rewriting is not properly configured on your server.<br />
+                    1) <a target="_blank" href="http://book.cakephp.org/3.0/en/installation.html#url-rewriting">Help me configure it</a><br />
+                    2) <a target="_blank" href="http://book.cakephp.org/3.0/en/development/configuration.html#general-configuration">I don't / can't use URL rewriting</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="row">
+    <div class="columns large-6">
+        <h3>Editing this Page</h3>
+        <ul>
+            <li class="bullet cutlery">To change the content of this page, edit: src/Template/Pages/home.ctp.</li>
+            <li class="bullet cutlery">You can also add some CSS styles for your pages at: webroot/css/.</li>
+        </ul>
+    </div>
+    <div class="columns large-6">
+        <h3>Getting Started</h3>
+        <ul>
+            <li class="bullet book"><a target="_blank" href="http://book.cakephp.org/3.0/en/">CakePHP 3.0 Docs</a></li>
+            <li class="bullet book"><a target="_blank" href="http://book.cakephp.org/3.0/en/tutorials-and-examples/bookmarks/intro.html">The 15 min Bookmarker Tutorial</a></li>
+            <li class="bullet book"><a target="_blank" href="http://book.cakephp.org/3.0/en/tutorials-and-examples/blog/blog.html">The 15 min Blog Tutorial</a></li>
+        </ul>
+        <p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="columns large-12 text-center">
+        <h3 class="more">More about Cake</h3>
+        <p>
+            CakePHP is a rapid development framework for PHP which uses commonly known design patterns like Front Controller and MVC.<br />
+            Our primary goal is to provide a structured framework that enables PHP users at all levels to rapidly develop robust web applications, without any loss to flexibility.
+        </p>
+    </div>
+    <hr/>
+</div>
+
+<div class="row">
+    <div class="columns large-4">
+        <i class="icon support">P</i>
+        <h3>Help and Bug Reports</h3>
+        <ul>
+            <li class="bullet cutlery">
+                <a href="irc://irc.freenode.net/cakephp">irc.freenode.net #cakephp</a>
+                <ul><li>Live chat about CakePHP</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="https://github.com/cakephp/cakephp/issues">CakePHP Issues</a>
+                <ul><li>CakePHP issues and pull requests</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://discourse.cakephp.org/">CakePHP Forum</a>
+                <ul><li>CakePHP official discussion forum</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="https://groups.google.com/group/cake-php">CakePHP Google Group</a>
+                <ul><li>Community mailing list</li></ul>
+            </li>
+        </ul>
+    </div>
+    <div class="columns large-4">
+        <i class="icon docs">r</i>
+        <h3>Docs and Downloads</h3>
+        <ul>
+            <li class="bullet cutlery">
+                <a href="http://api.cakephp.org/3.0/">CakePHP API</a>
+                <ul><li>Quick Reference</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://book.cakephp.org/3.0/en/">CakePHP Documentation</a>
+                <ul><li>Your Rapid Development Cookbook</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://bakery.cakephp.org">The Bakery</a>
+                <ul><li>Everything CakePHP</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://plugins.cakephp.org">CakePHP plugins repo</a>
+                <ul><li>A comprehensive list of all CakePHP plugins created by the community</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="https://github.com/cakephp/">CakePHP Code</a>
+                <ul><li>For the Development of CakePHP Git repository, Downloads</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="https://github.com/FriendsOfCake/awesome-cakephp">CakePHP Awesome List</a>
+                <ul><li>A curated list of amazingly awesome CakePHP plugins, resources and shiny things.</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://www.cakephp.org">CakePHP</a>
+                <ul><li>The Rapid Development Framework</li></ul>
+            </li>
+        </ul>
+    </div>
+    <div class="columns large-4">
+        <i class="icon training">s</i>
+        <h3>Training and Certification</h3>
+        <ul>
+            <li class="bullet cutlery">
+                <a href="http://cakefoundation.org/">Cake Software Foundation</a>
+                <ul><li>Promoting development related to CakePHP</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://training.cakephp.org/">CakePHP Training</a>
+                <ul><li>Learn to use the CakePHP framework</li></ul>
+            </li>
+            <li class="bullet cutlery">
+                <a href="http://certification.cakephp.org/">CakePHP Certification</a>
+                <ul><li>Become a certified CakePHP developer</li></ul>
+            </li>
+        </ul>
+    </div>
+</div>
+
+</body>
+</html>
