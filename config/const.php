a2d1757916437ab7abb5588391bd6f8b1f8f4d83
lucasgian <gianextensao@gmail.com>
2018-03-26 23:04:29 -0400

    feat (Router) update path: home.php (#16)

--- config/const.php
+++ config/const.php
@@ -1,5 +1,8 @@
 <?php
 
+define('__IMG__', 'images/');
+define('__MATERIALIZE__', 'src/Util/materialize/');
+
 define('__APP_ROOT__', '/idie-software/');
 define('__PRESENTER__', 'src/Presenter/');
 define('__COMPONENT__', 'src/Presenter/component/');
